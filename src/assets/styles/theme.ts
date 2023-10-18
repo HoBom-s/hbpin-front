0201119ea872340c2365f94ac0b4f68f2b6b9964
foxmon <jjockrod@naver.com>
2023-10-18 15:57:56 +0900

    style

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -9,9 +9,23 @@ export const theme: DefaultTheme = {
   border: {
     radius: {
       none: "none",
-      sm: "6px",
-      md: "8px",
-      lg: "10px",
+      sm: "3px",
+      md: "4px",
+      lg: "6px",
+    },
+  },
+  colors: {
+    black600: "#535353",
+    black700: "#3C3C3C",
+    black900: "#000000",
+    white700: "#FFFFFF",
+  },
+  font: {
+    weight: {
+      fw100: "100",
+      fw200: "200",
+      fw400: "400",
+      fw700: "700",
     },
   },
 };
