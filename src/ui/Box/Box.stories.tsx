7bcf8b7d25c79e7b35b06dc12b2f8dc5c84149d2
foxmon <jjockrod@naver.com>
2023-10-18 10:11:59 +0900

    storybook

---
+++ src/ui/Box/Box.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+// project
+import { Box } from "./Box";
+
+type Story = StoryObj<typeof Box>;
+
+const BoxStoryBook: Meta<typeof Box> = {
+  title: "Box",
+  component: Box,
+};
+
+export const Default: Story = {
+  args: {
+    className: "HoBom's Box component",
+    children: <span>Hello HoBom Box</span>,
+    sx: {
+      width: "300px",
+      height: "150px",
+      color: "pink",
+      border: "1px solid red",
+      borderRadius: "8px",
+      p: "8px",
+      m: "10px",
+    },
+  },
+};
+
+export default BoxStoryBook;
