f3583962bd9fdb880379d70136dec5ce21af7016
foxmon <jjockrod@naver.com>
2023-10-19 17:22:23 +0900

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import { useState } from "react";
-
 import { ThemeProvider } from "styled-components";
-
-import { FormLabel, TextField } from "@/ui";
 
 // assets
 import { GlobalStyle, theme } from "@/assets";
 
 function App() {
-  const [form, setForm] = useState({
-    "사용자 닉네임": "",
-  });
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <FormLabel label="사용자 닉네임" isRequired />
-      <TextField
-        name="사용자 닉네임"
-        value={form["사용자 닉네임"]}
-        onChange={(e) =>
-          setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-        }
-      />
+      안녕
     </ThemeProvider>
   );
 }
