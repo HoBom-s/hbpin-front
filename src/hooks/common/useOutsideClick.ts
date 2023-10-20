c171705999c25e06f667209e15491d02bb6407a8
foxmon <jjockrod@naver.com>
2023-10-20 14:06:46 +0900

    hooks

---
+++ src/hooks/common/useOutsideClick.ts
@@ -0,0 +1,35 @@
+import { useEffect, RefObject } from "react";
+
+interface OutsideClick {
+  ref: RefObject<HTMLElement>;
+  handler: () => void;
+}
+
+/**
+ * Outside click hook
+ *
+ * @example
+ *      useOutsideClick({
+ *          ref: ref,
+ *          handler: () => console.log("Hi"),
+ *      });
+ *
+ * @param {OutsideClick} param
+ */
+export const useOutsideClick = ({ ref, handler }: OutsideClick) => {
+  useEffect(() => {
+    document.addEventListener("click", (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handler();
+      }
+    });
+
+    return () => {
+      document.removeEventListener("click", (e: MouseEvent) => {
+        if (ref.current && !ref.current.contains(e.target as Node)) {
+          handler();
+        }
+      });
+    };
+  }, []);
+};
