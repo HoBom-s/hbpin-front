f8cdb75b35c0064298047f35f86b3fbf715393be
foxmon <jjockrod@naver.com>
2023-10-22 22:32:55 +0900

    use form

--- src/hooks/form/useForm.ts
+++ src/hooks/form/useForm.ts
@@ -22,15 +22,22 @@ interface FormValueReturn {
     const { formValue, handleFormValueChange } = useForm({
       username: {
         value: "",
-        errMsg: "",
+        type: "text",
+        errMsg: "이름은 2글자 이상 입력해 주세요.",
+        placeholder: "Please enter your username",
         isValid: false,
-        validate: (v: string | number) => (v as string).length > 2,
+        isRequired: true,
+        validate: (v: string | number) => (v as string).length >= 1,
       },
       password: {
         value: "",
-        errMsg: "",
+        type: "password",
+        errMsg:
+          "비밀번호는 영문, 숫자, 특수기호를 조합하여 8 ~ 15자리로 입력해 주세요.",
+        placeholder: "Please enter your username",
         isValid: false,
-        validate: (v: string | number) => (v as string).length > 2,
+        isRequired: true,
+        validate: (v: string | number) => isPassword(v as string),
       },
     });
  * 
