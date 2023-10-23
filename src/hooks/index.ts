13561dbc0cfa4b675548993d10637d192cbf9c69
foxmon <jjockrod@naver.com>
2023-10-23 14:49:58 +0900

    hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,26 @@
+// common
+import { useObserver } from "./common/useObserver";
 import { useOutsideClick } from "./common/useOutsideClick";
 import { useRipple } from "./common/useRipple";
 
+// errors
 import { useThrowAsyncError } from "./errors/useThrowAsyncError";
 
+// form
 import { useForm } from "./form/useForm";
+
+// img
+import { useLazyLoadImage } from "./img/useLazyLoadImage";
 
 // type
 import type { FormValue } from "./form/useForm";
 
-export { useOutsideClick, useRipple, useThrowAsyncError, useForm, FormValue };
+export {
+  useObserver,
+  useOutsideClick,
+  useRipple,
+  useThrowAsyncError,
+  useForm,
+  useLazyLoadImage,
+  FormValue,
+};
