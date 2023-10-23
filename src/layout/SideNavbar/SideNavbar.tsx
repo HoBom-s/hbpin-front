ef74777ff405b4dfa3c22f5e0bfaafce1c91187a
foxmon <jjockrod@naver.com>
2023-10-23 22:56:43 +0900

    sidebar

--- src/layout/SideNavbar/SideNavbar.tsx
+++ src/layout/SideNavbar/SideNavbar.tsx
@@ -1,5 +1,115 @@
+import {
+  useCallback,
+  useState,
+  MouseEvent,
+  ChangeEvent,
+  FormEvent,
+} from "react";
 import { SideNavbarWrapper } from "./style";
 
+// assets
+import { theme } from "@/assets";
+
+// project
+import {
+  Box,
+  Tabs,
+  Tab,
+  FormControl,
+  TextField,
+  Button,
+  Text,
+  Divider,
+} from "@/ui";
+
+interface TabItem {
+  key: string;
+  title: string;
+}
+
 export const SideNavbar = () => {
-  return <SideNavbarWrapper $isOpen={true}>ㅎㅇㅎㅇ</SideNavbarWrapper>;
+  const tabItems: TabItem[] = [
+    {
+      key: "scheduled",
+      title: "방문예정",
+    },
+    {
+      key: "completed",
+      title: "방문완료",
+    },
+    {
+      key: "bookmark",
+      title: "즐겨찾기",
+    },
+  ];
+
+  const [tabValue, setTabValue] = useState<TabItem>(tabItems[0]);
+  const [keyword, setKeyword] = useState<string>("");
+
+  const handleTabValueChange = useCallback(
+    (_: MouseEvent, selectedTabItem: TabItem) => {
+      setTabValue(selectedTabItem);
+    },
+    [],
+  );
+
+  const handleKeywordChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+
+      setKeyword(value);
+    },
+    [],
+  );
+
+  const handleKeywordSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <SideNavbarWrapper $isOpen={true}>
+      <Box sx={{ p: "10px" }}>
+        <Text sx={{ fontWeight: theme.font.weight.fw700, fontSize: "22px" }}>
+          HoBom PIN
+        </Text>
+      </Box>
+      <Divider sx={{ mb: "10px" }} />
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Tabs
+          sx={{ width: "100%" }}
+          value={tabValue}
+          onChange={handleTabValueChange}
+        >
+          {tabItems.map((tab: TabItem) => (
+            <Tab key={tab.key} label={tab.title} value={tab} />
+          ))}
+        </Tabs>
+      </Box>
+      <FormControl sx={{ p: "0px", pt: "20px" }} onSubmit={handleKeywordSubmit}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            gap: "4px",
+          }}
+        >
+          <TextField
+            type="text"
+            placeholder="검색어를 입력해 주세요."
+            name="keyword"
+            value={keyword}
+            onChange={handleKeywordChange}
+          />
+          <Button
+            type="submit"
+            variant="fill"
+            label="검색"
+            size="sm"
+            sx={{ width: "80px" }}
+          />
+        </Box>
+      </FormControl>
+    </SideNavbarWrapper>
+  );
 };
--- src/layout/SideNavbar/style.ts
+++ src/layout/SideNavbar/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const SIDE_NAV_BAR_WIDTH: number = 256;
+const SIDE_BAR_NAV_WIDTH: number = 291;
 const ICON_WIDTH: number = 32;
 
 interface SideNavbarWrapperProps {
@@ -9,9 +9,13 @@ interface SideNavbarWrapperProps {
 
 export const SideNavbarWrapper = styled.div<SideNavbarWrapperProps>`
   margin: 0;
-  width: ${(props) => (props.$isOpen ? SIDE_NAV_BAR_WIDTH : ICON_WIDTH)}px;
+  padding: 10px;
   min-height: 100vh;
+  width: ${(props) => (props.$isOpen ? "auto" : `${ICON_WIDTH}px`)};
+  max-width: ${SIDE_BAR_NAV_WIDTH}px;
   background-color: ${(props) => props.theme.colors.white700};
-  border-right: 1px solid ${(props) => props.theme.colors.gray200};
   transition: width 0.3s;
+  box-shadow:
+    0 1px 2px rgba(60, 64, 67, 0.3),
+    0 2px 3px 2px rgba(60, 64, 67, 0.15);
 `;
