fc17d1643a82ba9a88ece2171e8efebbb1da88de
foxmon <jjockrod@naver.com>
2023-10-24 11:37:01 +0900

    Side nav bar

--- src/layout/SideNavbar/SideNavbar.tsx
+++ src/layout/SideNavbar/SideNavbar.tsx
@@ -21,6 +21,7 @@ import {
   Text,
   Divider,
 } from "@/ui";
+import { Spot } from "@/components";
 
 interface TabItem {
   key: string;
@@ -68,48 +69,120 @@ export const SideNavbar = () => {
 
   return (
     <SideNavbarWrapper $isOpen={true}>
-      <Box sx={{ p: "10px" }}>
-        <Text sx={{ fontWeight: theme.font.weight.fw700, fontSize: "22px" }}>
-          HoBom PIN
-        </Text>
-      </Box>
-      <Divider sx={{ mb: "10px" }} />
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Tabs
-          sx={{ width: "100%" }}
-          value={tabValue}
-          onChange={handleTabValueChange}
-        >
-          {tabItems.map((tab: TabItem) => (
-            <Tab key={tab.key} label={tab.title} value={tab} />
-          ))}
-        </Tabs>
-      </Box>
-      <FormControl sx={{ p: "0px", pt: "20px" }} onSubmit={handleKeywordSubmit}>
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            gap: "4px",
-          }}
-        >
-          <TextField
-            type="text"
-            placeholder="검색어를 입력해 주세요."
-            name="keyword"
-            value={keyword}
-            onChange={handleKeywordChange}
-          />
-          <Button
-            type="submit"
-            variant="fill"
-            label="검색"
-            size="sm"
-            sx={{ width: "80px" }}
-          />
+      <div>
+        <Box sx={{ p: "10px" }}>
+          <Text sx={{ fontWeight: theme.font.weight.fw700, fontSize: "22px" }}>
+            HoBom PIN
+          </Text>
         </Box>
-      </FormControl>
+        <Divider sx={{ mb: "10px" }} />
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Tabs
+            sx={{ width: "100%" }}
+            value={tabValue}
+            onChange={handleTabValueChange}
+          >
+            {tabItems.map((tab: TabItem) => (
+              <Tab key={tab.key} label={tab.title} value={tab} />
+            ))}
+          </Tabs>
+        </Box>
+        <FormControl
+          sx={{ p: "0px", pt: "20px", pb: "20px" }}
+          onSubmit={handleKeywordSubmit}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              gap: "4px",
+            }}
+          >
+            <TextField
+              type="text"
+              placeholder="검색어를 입력해 주세요."
+              name="keyword"
+              value={keyword}
+              onChange={handleKeywordChange}
+            />
+            <Button
+              type="submit"
+              variant="fill"
+              label="검색"
+              size="sm"
+              sx={{ width: "80px" }}
+            />
+          </Box>
+        </FormControl>
+        <Divider sx={{ mb: "10px" }} />
+      </div>
+      <Box sx={{ height: "calc(100vh - 240px)", overflowY: "auto" }}>
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+        <Spot
+          name="호랑이카페"
+          rating={5}
+          spotImageUrl="https://picsum.photos/id/1/500/500"
+          address="중구 을지로 157"
+          business="영업 중 ⋅ 오후 9:00에 영업 종료"
+          mealKind="매장 내 식사 ⋅ 테이크아웃"
+        />
+      </Box>
     </SideNavbarWrapper>
   );
 };
--- src/layout/SideNavbar/style.ts
+++ src/layout/SideNavbar/style.ts
@@ -10,7 +10,9 @@ interface SideNavbarWrapperProps {
 export const SideNavbarWrapper = styled.div<SideNavbarWrapperProps>`
   margin: 0;
   padding: 10px;
+  height: 100%;
   min-height: 100vh;
+  max-height: 100vh;
   width: ${(props) => (props.$isOpen ? "auto" : `${ICON_WIDTH}px`)};
   max-width: ${SIDE_BAR_NAV_WIDTH}px;
   background-color: ${(props) => props.theme.colors.white700};
@@ -18,4 +20,5 @@ export const SideNavbarWrapper = styled.div<SideNavbarWrapperProps>`
   box-shadow:
     0 1px 2px rgba(60, 64, 67, 0.3),
     0 2px 3px 2px rgba(60, 64, 67, 0.15);
+  position: fixed;
 `;
