cb6abde7d525449dfbf1621f531fd2050eaa1773
foxmon <jjockrod@naver.com>
2023-10-24 15:28:45 +0900

    components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 // ErrorBoundary
+import { GeoMapFallback } from "./ErrorBoundary/Fallback/GeoMapFallback";
 import { GlobalFallback } from "./ErrorBoundary/Fallback/GlobalFallback";
+import { GeoMapErrorBoundary } from "./ErrorBoundary/GeoMapErrorBoundary";
 import { GlobalErrorBoundary } from "./ErrorBoundary/GlobalErrorBoundary";
 
 // Icons
@@ -11,14 +13,22 @@ import { ImmigrationForm } from "./ImmigrationForm/ImmigrationForm";
 // ListRenderer
 import { ListRenderer } from "./ListRenderer/ListRenderer";
 
+// Map
+import { Map } from "./Map/Map";
+import { MyGeoMapComponent } from "./Map/GeoMapComponent/GeoMapComponent";
+
 // Spot
 import { Spot } from "./Spot/Spot";
 
 export {
+  GeoMapFallback,
   GlobalFallback,
+  GeoMapErrorBoundary,
   GlobalErrorBoundary,
   Icon,
   ImmigrationForm,
   ListRenderer,
+  Map,
+  MyGeoMapComponent,
   Spot,
 };
