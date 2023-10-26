139b6c4b90a35028258a1ac81b2d69b9c4a649c2
foxmon <jjockrod@naver.com>
2023-10-26 14:35:57 +0900

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import viteCompression from "vite-plugin-compression";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import * as path from "path";
 
-console.log(process.env.SENTRY_AUTH_TOKEN);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
