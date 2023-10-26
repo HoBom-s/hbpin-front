0a22d79576461b0c8174201f83a2c3e37a0854a3
foxmon <jjockrod@naver.com>
2023-10-26 14:29:55 +0900

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // gzip 최적화
     viteCompression({
       ext: ".ts",
       algorithm: "gzip",
     }),
+
+    // gzip 최적화
     viteCompression({
       ext: ".tsx",
       algorithm: "gzip",
+    }),
+
+    // Sentry vite plugin
+    sentryVitePlugin({
+      org: "hobom",
+      project: "hbpin-front",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      sourcemaps: {
+        assets: "./build/**",
+        filesToDeleteAfterUpload: "./build/**/*.map",
+      },
     }),
   ],
   server: {
@@ -24,5 +39,8 @@ export default defineConfig({
   },
   build: {
     outDir: path.join(__dirname, "build"),
+
+    // Build 후 기존 소스코드 원복 불가하게 Sentry를 위해 켜야하는 옵션
+    sourcemap: true,
   },
 });
